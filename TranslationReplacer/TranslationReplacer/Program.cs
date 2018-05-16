7ea1a84ae7faa077ad28a49ad4bafdde9a7cb8fd
polar.rafal.work <polar.rafal.work@gmail.com>
2018-05-16 14:12:22 +0200

    Added relesase version with parameters from console

--- TranslationReplacer/TranslationReplacer/Program.cs
+++ TranslationReplacer/TranslationReplacer/Program.cs
@@ -13,12 +13,31 @@ namespace TranslationReplacer
     {
         static void Main(string[] args)
         {
+#if !DEBUG
+            if(args.Length == 2)
+            {
+                string fileSourcePath = args[0];
+                string fileOutputPath = args[1];
+                Translator translator = new Translator(fileSourcePath, fileOutputPath);
+                translator.TranslateData();
+
+                Console.WriteLine("Please enter any key...");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Incorrect number of parametres (accepted parameters: sourceFilePath, outputFilePath)");
+                Console.WriteLine("Please enter any key...");
+                Console.ReadLine();
+            }
+#else
             string fileSourcePath = @"C:/Users/rpolar/Desktop/GIT/TranslationFinder/TranslationReplacer/TranslationReplacer/SourceTranslationFiles/locale-de-DE.json";
             string fileOutputPath = @"C:/Users/rpolar/Desktop/GIT/TranslationFinder/TranslationReplacer/TranslationReplacer/SourceTranslationFiles/locale-de-DE_output.json";
             Translator translator = new Translator(fileSourcePath, fileOutputPath);
             translator.TranslateData();
-            
+
             Console.ReadLine();
+#endif
         }
     }
 }
